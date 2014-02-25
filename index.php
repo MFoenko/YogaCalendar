f5232bd460924cd08af87f1ba34444e6f512fafa
MFoenko <=>
2014-02-25 11:45:33 -0500

    index somehow went back in time...

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ $mysql_host = "mysql5.000webhost.com"; $mysql_database = "a1631389_assin"; $mysq
 	session_start();
 	require("php/scripts/connect_to_database.php");
 
-	if (!isset($_SESSION['user']))
+	if (!isset($_SESSION['cal_user']))
 		require("php/templates/loginScreen.php");
 	else
-		require("php/templates/home.php");
+		require("php/templates/homeWnav.php");
 ?>
